6e7b08f7f1a2d1aaba47154e39a22b06d93e652c
Sebastiaan Dammann <sebastiaandammann@outlook.com>
2019-04-29 20:41:09 +0200

    Rename to PSMenu (convention) and rename function

---
+++ PSMenu.psm1
@@ -0,0 +1,132 @@
+function DrawMenu {
+    param ([array]$menuItems, $menuPosition, $Multiselect, $selection, [array]$commands)
+    $l = $menuItems.length
+    $wh = (Get-Host).UI.RawUI.WindowSize.Height - 2;
+    $ww = (Get-Host).UI.RawUI.WindowSize.Width;
+    $idx = 0;
+
+    if ($l -gt $wh) {
+        $l = $wh;
+        if ($menuPosition -gt $l) {
+            $idx = $menuPosition - $l;
+        }
+    }
+    for ($i = 0; $i -le $l; $i++) {
+        if ($menuItems[$idx] -ne $null) {
+            $item = $menuItems[$idx]
+            if ($Multiselect) {
+                if ($selection -contains $idx) {
+                    $item = '[x] ' + $item
+                }
+                else {
+                    $item = '[ ] ' + $item
+                }
+            }
+            #pad the text to ensure no artifacts when scrolling list
+            $menuText = $item.PadRight($ww - ($item.Length + 2), ' ');
+            if ($idx -eq $menuPosition) {
+                Write-Host "> $menuText" -ForegroundColor Green
+            }
+            else {
+                Write-Host "  $menuText"
+            }
+        }
+        $idx++;
+    }
+    if ($commands.Count -ne 0) {
+        Write-Host ($commands -join ", ")
+    }
+}
+
+function Toggle-Selection {
+    param ($pos, [array]$selection)
+    if ($selection -contains $pos) { 
+        $result = $selection | where { $_ -ne $pos }
+    }
+    else {
+        $selection += $pos
+        $result = $selection
+    }
+    $result
+}
+
+function Show-Menu {
+    param ([array]$menuItems, [switch]$ReturnIndex = $false, [switch]$Multiselect, [array]$commands)
+    $vkeycode = 0
+    $pos = 0
+    $cmd = ''
+    $cmdHash = @{ }
+    $cmdText = @(0) * $commands.Count
+    if ($commands.Count -ne 0) {
+        for ($i = 0; $commands.Count - 1; $i++) {
+            $c = $commands[$i];
+            if ($c -eq $null) {
+                break;
+            }		
+            $k = $c[0];
+            $cmdHash[$k] = $c;
+            $cmdText[$i] = '[' + $k + ']' + $c.Substring(1);		
+        }
+    }
+    $selection = @()
+    $cur_pos = [System.Console]::CursorTop
+    [console]::CursorVisible = $false #prevents cursor flickering
+    if ($menuItems.Length -gt 0) {
+        DrawMenu $menuItems $pos $Multiselect $selection $cmdText
+        While ($vkeycode -ne 13 -and $vkeycode -ne 27 -and $cmd -eq '') {
+            $press = $host.ui.rawui.readkey("NoEcho,IncludeKeyDown")
+            $vkeycode = $press.virtualkeycode
+            If ($vkeycode -eq 38 -or $press.Character -eq 'k') { $pos-- }
+            If ($vkeycode -eq 40 -or $press.Character -eq 'j') { $pos++ }			
+            If ($press.Character -eq ' ') { $selection = Toggle-Selection $pos $selection }
+            If ($pos -lt 0) { $pos = 0 }
+            If ($vkeycode -eq 27) { $pos = $null }
+            If ($pos -ge $menuItems.length) { $pos = $menuItems.length - 1 }
+            If ($commands.Count -ne 0) {
+                foreach ($key in $cmdHash.keys) {
+                    If ($press.Character -eq $key) { 
+                        $val = $cmdHash[$key];
+                        $cmd = $val;
+                        break; 
+                    }
+                }
+            }
+            If ($vkeycode -ne 27) {
+                [System.Console]::SetCursorPosition(0, $cur_pos)
+                DrawMenu $menuItems $pos $Multiselect $selection $cmdText
+            }
+        }
+    }
+    else {
+        $pos = $null
+    }
+    [console]::CursorVisible = $true
+
+    if ($ReturnIndex -eq $false -and $pos -ne $null) {
+        if ($Multiselect) {
+            return get-SelectedValue -cmd $cmd -value $menuItems[$selection]
+			
+        }
+        else {
+            return get-SelectedValue -cmd $cmd -value $menuItems[$pos]
+        }
+    }
+    else {
+        if ($Multiselect) {
+            return get-SelectedValue -cmd $cmd -value $selection
+        }
+        else {
+            return get-SelectedValue -cmd $cmd -value $pos
+        }
+    }
+}
+
+function get-SelectedValue {
+    Param($cmd, $value)
+    if ($cmd -ne '') {
+        return @($cmd, @($value));
+    }
+    else {
+        return $value;
+    }
+}
--- README.md
+++ README.md
@@ -1,38 +1,35 @@
-# PS-Menu
+# PSMenu
+
 Simple module to generate interactive console menus (like yeoman)
 
 # Examples:
 
 ```powershell
-menu @("option 1", "option 2", "option 3")
+Show-Menu @("option 1", "option 2", "option 3")
 ```
- ![Example](https://github.com/chrisseroka/ps-menu/raw/master/docs/example1.gif)
-
-More useful example:
-
- ![Example](https://github.com/chrisseroka/ps-menu/raw/master/docs/example2.gif)
 
 # Installation
 
 You can install it from the PowerShellGallery using PowerShellGet
 
 ```powershell
-Install-Module PS-Menu
+Install-Module PSMenu
 ```
+
 # Features
 
-* Returns value of selected menu item
-* Returns index of selected menu item (using `-ReturnIndex` switch)
-* Returns an array of items (using `-MultiSelect` switch)
-* Display an array of commands with list items (using `-commands` param)
-* Navigation with `up/down` arrows
-* Navigation with `j/k` (vim style)
-* Longer list scroll within window
-* Esc key quits the menu (`null` value returned)
+- Returns value of selected menu item
+- Returns index of selected menu item (using `-ReturnIndex` switch)
+- Returns an array of items (using `-MultiSelect` switch)
+- Display an array of commands with list items (using `-commands` param)
+- Navigation with `up/down` arrows
+- Navigation with `j/k` (vim style)
+- Longer list scroll within window
+- Esc key quits the menu (`null` value returned)
 
 # Contributing
 
-* Source hosted at [GitHub][repo]
-* Report issues/questions/feature requests on [GitHub Issues][issues]
+- Source hosted at [GitHub][repo]
+- Report issues/questions/feature requests on [GitHub Issues][issues]
 
-Pull requests are very welcome! 
+Pull requests are very welcome!
