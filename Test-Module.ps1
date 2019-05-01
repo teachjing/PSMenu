26e7172982ed9ca8fdabfde3462a3a7c00eba0db
Sebastiaan Dammann <sebastiaandammann@outlook.com>
2019-05-01 18:04:38 +0200

    Allow easier testing of module

---
+++ Test-Module.ps1
@@ -0,0 +1,57 @@
+param([Switch]$NoNewShell)
+
+$MyPath = $PSScriptRoot
+
+if ($NoNewShell -eq $false) {
+    Write-Host "Opening new shell..."
+    & powershell @("-NoExit", "-File", "$MyPath\Test-Module.ps1", "-NoNewShell")
+    Exit $LASTEXITCODE
+}
+
+Import-Module .\src\PSMenu.psm1 -Verbose
+
+function Test-MenuWithStringOptions() {
+    $Opts = @(
+        "Cheesecake",
+        "Fries",
+        "Yoghurt"
+    )
+
+    $Chosen = Show-Menu -MenuItems $Opts
+
+    Write-Host "You chose: $Chosen"
+}
+
+class MyMenuOption {
+    [String]$DisplayName
+    [ScriptBlock]$ExecuteCallback
+
+    [String]ToString() {
+        Return $This.DisplayName
+    }
+
+    [Void]Execute() {
+        & $This.ExecuteCallback
+    }
+}
+
+function New-MenuItem([String]$DisplayName, [ScriptBlock]$ExecuteCallback) {
+    $MenuItem = [MyMenuOption]::new()
+    $MenuItem.DisplayName = $DisplayName
+    $MenuItem.ExecuteCallback = $ExecuteCallback
+    Return $MenuItem
+}
+
+function Test-MenuWithClassOptions() {
+    $Opts = @(
+        $(New-MenuItem -DisplayName "Say Hello" -ExecuteCallback { Write-Host "Hello!" }),
+        $(New-MenuItem -DisplayName "Say Bye!" -ExecuteCallback { Write-Host "Bye!" })
+    )
+
+    $Chosen = Show-Menu -MenuItems $Opts
+    Write-Host "You chose: $Chosen"
+
+    Write-Host ""
+    $Chosen.Execute()
+    Write-Host ""
+}
