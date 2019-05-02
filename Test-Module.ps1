39afb0ca701f985abc2c58dfbb8b2b7ab905989f
Sebastiaan Dammann <sebastiaandammann@outlook.com>
2019-05-02 20:06:30 +0200

    Fix path after move

--- Test-Module.ps1
+++ Test-Module.ps1
@@ -30,7 +30,7 @@ if ($NoNewShell -eq $false) {
 }
 
 Clear-Host
-Import-Module .\src\PSMenu\PSMenu.psm1 -Verbose
+Import-Module .\PSMenu\PSMenu.psm1 -Verbose
 
 function Test-MenuWithStringOptions() {
     $Opts = @(
