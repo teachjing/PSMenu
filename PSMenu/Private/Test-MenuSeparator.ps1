3018d72187365835b04e8174f898edb09688527e
Sebastiaan Dammann <sebastiaandammann@outlook.com>
2022-06-20 19:51:11 +0200

    Improve handling of array menu items - fixes #12

--- PSMenu/Private/Test-MenuSeparator.ps1
+++ PSMenu/Private/Test-MenuSeparator.ps1
@@ -2,5 +2,5 @@ function Test-MenuSeparator([Parameter(Mandatory)] $MenuItem) {
     $Separator = Get-MenuSeparator
 
     # Separator is a singleton and we compare it by reference
-    Return $MenuItem -eq $Separator
+    Return [Object]::ReferenceEquals($Separator, $MenuItem)
 }
